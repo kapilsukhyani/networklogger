b269c517f415b1625e39c8762358fb10b87d23fa
unknown <kapil.sukhyani@ISPUN_011.infostretch.com>
2014-04-22 17:55:49 +0530

    Committing chnages

--- src/com/enlighten/transparentproxy/app/ProxyApplication.java
+++ src/com/enlighten/transparentproxy/app/ProxyApplication.java
@@ -5,10 +5,15 @@ import java.lang.Thread.UncaughtExceptionHandler;
 import java.util.concurrent.TimeoutException;
 
 import android.app.Application;
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.content.pm.ApplicationInfo;
 import android.content.pm.PackageManager;
 import android.content.pm.PackageManager.NameNotFoundException;
 import android.content.res.Configuration;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.util.Log;
 
 import com.enlighten.transparentproxy.constants.Constants;
@@ -51,50 +56,172 @@ public class ProxyApplication extends Application {
 		super.onTerminate();
 	}
 
-	public void initIPTable() {
-		try {
-			ApplicationInfo info = getPackageManager().getApplicationInfo(
-					getPackageName(), PackageManager.GET_META_DATA);
+	public boolean initIPTable(final Handler handler) {
+		final SharedPreferences sharedPreferences = getSharedPreferences(
+				Constants.Shared_Preferences, Context.MODE_PRIVATE);
 
-			CommandCapture initCommand = new CommandCapture(
-					Constants.IPTABLES_INIT_COMMAND_ID,
-					Constants.IPTABLES_INIT_COMMAND.replace(Constants.UID,
-							info.uid + "")) {
+		boolean initiated = sharedPreferences.getBoolean(
+				Constants.IPTABLE_INITIATED, false);
+		if (!initiated) {
+			try {
+
+				ApplicationInfo info = getPackageManager().getApplicationInfo(
+						getPackageName(), PackageManager.GET_META_DATA);
+
+				String command = Constants.IPTABLES_INIT_COMMAND.replace(
+						Constants.UID, String.valueOf(info.uid));
+				AppLog.logDebug(TAG, "running command " + command);
+				CommandCapture initCommand = new CommandCapture(
+						Constants.IPTABLES_INIT_COMMAND_ID, command) {
+
+					@Override
+					protected void commandFinished() {
+						super.commandFinished();
+						String result = this.toString();
+
+						AppLog.logDebug(TAG, "Init Ip Table commad result "
+								+ result);
+
+						// TODO: write following logic in commandCompleted as it
+						// returns a proper exitcode which can be compared to
+						// see the response of iptables
+						final Message message = Message.obtain(handler,
+								Constants.IPTABLE_INITIATED_MESSAGE_ID);
+						Bundle obj = new Bundle();
+						/*
+						 * try { int res = Integer.valueOf(result);
+						 * 
+						 * // iptables returns zero when ran properly if (res ==
+						 * 0) {
+						 */
+						sharedPreferences.edit()
+								.putBoolean(Constants.IPTABLE_INITIATED, true)
+								.commit();
+
+						obj.putBoolean(Constants.IPTABLE_INITIATED, true);
+
+						/*
+						 * } else { AppLog.logDebug(TAG, "something went wrong "
+						 * + result);
+						 * obj.putBoolean(Constants.IPTABLE_INITIATED, false);
+						 * obj.putString(Constants.FAILURE_REASON,
+						 * "Error while running iptables");
+						 * 
+						 * } } catch (NumberFormatException exception) {
+						 * obj.putBoolean(Constants.IPTABLE_INITIATED, false);
+						 * obj.putString(Constants.FAILURE_REASON,
+						 * "Iptables not available");
+						 * 
+						 * }
+						 */
+
+						message.setData(obj);
+
+						handler.post(new Runnable() {
+
+							@Override
+							public void run() {
+								handler.handleMessage(message);
+
+							}
+						});
+
+					}
+
+					@Override
+					public void commandTerminated(int id, String reason) {
+						super.commandTerminated(id, reason);
+						AppLog.logDebug(TAG, "Init Ip Table commad terminated "
+								+ reason);
+					}
+
+					@Override
+					public void commandCompleted(int id, int exitcode) {
+						super.commandCompleted(id, exitcode);
+						AppLog.logDebug(TAG, "Init Ip Table commad completed "
+								+ exitcode);
+					}
+				};
+
+				RootTools.getShell(true).add(initCommand);
+				return false;
+			} catch (IOException e) {
+				e.printStackTrace();
+			} catch (TimeoutException e) {
+				e.printStackTrace();
+			} catch (RootDeniedException e) {
+				e.printStackTrace();
+			} catch (NameNotFoundException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			}
+		}
+		
+		return true;
+
+	}
+
+	public void initAppLevelFilter(final Handler handler) {
+		final SharedPreferences sharedPreferences = getSharedPreferences(
+				Constants.Shared_Preferences, Context.MODE_PRIVATE);
+		boolean filterEnabled = sharedPreferences.getBoolean(
+				Constants.FILTER_ENABLED, false);
+		if (filterEnabled) {
+			// TODO delete previous filter first then call from commandCompleted
+
+		} else {
+			addSytemLevelFilter(handler);
+		}
+
+	}
+
+	private void addSytemLevelFilter(final Handler handler) {
+		final SharedPreferences sharedPreferences = getSharedPreferences(
+				Constants.Shared_Preferences, Context.MODE_PRIVATE);
+
+		try {
+			final String command = Constants.SYSTEM_LEVEL_FILTER_COMMAND;
+			AppLog.logDebug(TAG, "running command " + command);
+			CommandCapture appFilterCommand = new CommandCapture(
+					Constants.IPTABLES_INIT_COMMAND_ID, command) {
+				@Override
+				public void commandCompleted(int id, int exitcode) {
+					super.commandCompleted(id, exitcode);
+					sharedPreferences.edit()
+							.putBoolean(Constants.FILTER_ENABLED, true)
+							.putString(Constants.FILTER, command);
+
+					final Message message = Message.obtain(handler,
+							Constants.SYSTEM_LEVEL_FILTER_ENABLED_MESSAGE_ID);
+					Bundle obj = new Bundle();
+
+					obj.putBoolean(Constants.FILTER_ENABLED, true);
+
+					message.setData(obj);
+
+					handler.post(new Runnable() {
+
+						@Override
+						public void run() {
+							handler.handleMessage(message);
+
+						}
+					});
+				}
 
 				@Override
 				protected void commandFinished() {
 					super.commandFinished();
-					String result = this.toString();
-					AppLog.logDebug(TAG, "Init Ip Table commad result "
-							+ result);
-
-				}
-
-				@Override
-				public void commandTerminated(int id, String reason) {
-					super.commandTerminated(id, reason);
-					AppLog.logDebug(TAG, "Init Ip Table commad terminated "
-							+ reason);
-				}
-
-				@Override
-				public void commandCompleted(int id, int exitcode) {
-					super.commandCompleted(id, exitcode);
-					AppLog.logDebug(TAG, "Init Ip Table commad completed "
-							+ exitcode);
 				}
 			};
 
-			RootTools.getShell(true).add(initCommand);
+			RootTools.getShell(true).add(appFilterCommand);
 		} catch (IOException e) {
 			e.printStackTrace();
 		} catch (TimeoutException e) {
 			e.printStackTrace();
 		} catch (RootDeniedException e) {
 			e.printStackTrace();
-		} catch (NameNotFoundException e1) {
-			// TODO Auto-generated catch block
-			e1.printStackTrace();
 		}
 
 	}
--- src/com/enlighten/transparentproxy/constants/Constants.java
+++ src/com/enlighten/transparentproxy/constants/Constants.java
@@ -9,14 +9,28 @@ public class Constants {
 	public static final String PREF_PLAYSOUND = "prefPlaysound";
 	public static final String PREF_RINGTONE = "prefMessageringtone";
 	public static final String PREF_SERVER_PORT = "prefServerPort";
-	public static final String UID = "[uid]";
+	public static final String UID = "[UID]";
 
 	public static final int DEFAULT_SERVER_PORT = 8080;
 
 	public static final String APP_OWNER = "user_id";
 	public static final int PS_COMMAND_ID = 0x1;
 	public static final String PS_COMMAND = "ps | grep com.enlighten.transparentproxy";
-	
+
 	public static final int IPTABLES_INIT_COMMAND_ID = 0x2;
 	public static final String IPTABLES_INIT_COMMAND = "iptables -t nat -I OUTPUT -m owner --uid-owner [UID] -j ACCEPT";
+	public static final String SYSTEM_LEVEL_FILTER_COMMAND = "iptables -t nat -A OUTPUT -p 6 --dport 80 -j DNAT --to 127.0.0.1:8080";
+	public static final String Shared_Preferences = "app_preferences";
+	public static final String IPTABLE_INITIATED = "iptable_initiated";
+
+	public static final int IPTABLE_INITIATED_MESSAGE_ID = 0x1;
+	public static final int SYSTEM_LEVEL_FILTER_ENABLED_MESSAGE_ID = 0x2;
+
+	public static final String FAILURE_REASON = "failure_reason";
+
+	public static final String FILTER_ENABLED = "filter_enabled";
+
+	public static final String FILTER = "current_filter";
+
+	
 }
